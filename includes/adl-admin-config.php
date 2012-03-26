6dc9b88007baa418441ae9c5cf79dd2cc662b56a
dartiss <dartiss@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-03-26 13:57:11 +0000

    git-svn-id: https://plugins.svn.wordpress.org/simple-draft-list/tags/2.0.2@523825 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/adl-admin-config.php
@@ -0,0 +1,32 @@
+<?php
+/**
+* Admin Menu Functions
+*
+* Various functions relating to the various administration screens
+*
+* @package	Artiss-Draft-List
+*/
+
+/**
+* Add meta to plugin details
+*
+* Add options to plugin meta line
+*
+* @since	2.0
+*
+* @param	string  $links	Current links
+* @param	string  $file	File in use
+* @return   string			Links, now with settings added
+*/
+
+function adl_set_plugin_meta( $links, $file ) {
+
+	if ( strpos( $file, 'simple-draft-list.php' ) !== false ) {
+		$links = array_merge( $links, array( '<a href="http://www.artiss.co.uk/forum">' . __( 'Support' ) . '</a>' ) );
+		$links = array_merge( $links, array( '<a href="http://www.artiss.co.uk/donate">' . __( 'Donate' ) . '</a>' ) );
+	}
+
+	return $links;
+}
+add_filter( 'plugin_row_meta', 'adl_set_plugin_meta', 10, 2 );
+?>
